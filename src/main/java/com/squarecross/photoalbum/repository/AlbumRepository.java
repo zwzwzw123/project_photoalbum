b4c5fa6f6b657a507042837f910f9c302079d652
jooyoun lee <115621310+zwzwzw123@users.noreply.github.com>
2023-04-07 09:52:19 +0900

    implement Album Repository Interface

---
+++ src/main/java/com/squarecross/photoalbum/repository/AlbumRepository.java
@@ -0,0 +1,9 @@
+package com.squarecross.photoalbum.repository;
+
+import com.squarecross.photoalbum.domain.Album;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlbumRepository extends JpaRepository<Album, Long> {
+}
