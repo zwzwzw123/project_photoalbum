ab20934c7d496479cb0522823b13be8d80159a15
jooyoun lee <115621310+zwzwzw123@users.noreply.github.com>
2023-04-07 09:58:31 +0900

    implement Album Service

---
+++ src/main/java/com/squarecross/photoalbum/service/AlbumService.java
@@ -0,0 +1,25 @@
+package com.squarecross.photoalbum.service;
+
+import com.squarecross.photoalbum.domain.Album;
+import com.squarecross.photoalbum.repository.AlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+@Service
+public class AlbumService {
+
+    @Autowired
+    private AlbumRepository albumRepository;
+
+    public Album getAlbum(Long albumId){
+        Optional<Album> res = albumRepository.findById(albumId);
+        if(res.isPresent()){
+            return res.get();
+        }else{
+            throw new EntityNotFoundException(String.format("앨범 아이디 %d로 조회되지 않았습니다.",albumId));
+        }
+    }
+}
