5033db7be968c5f74a70c201cc18bf8cbc1d279f
jooyoun lee <115621310+zwzwzw123@users.noreply.github.com>
2023-04-07 10:06:27 +0900

    implement AlbumServiceTest

---
+++ src/test/java/com/squarecross/photoalbum/service/AlbumServiceTest.java
@@ -0,0 +1,33 @@
+package com.squarecross.photoalbum.service;
+
+import com.squarecross.photoalbum.domain.Album;
+import com.squarecross.photoalbum.repository.AlbumRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class AlbumServiceTest {
+
+    @Autowired
+    AlbumRepository albumRepository;
+
+    @Autowired
+    AlbumService albumService;
+
+    @Test
+    void getAlbum() {
+        Album album = new Album();
+        album.setAlbumName("테스트");
+        Album saveAlbum = albumRepository.save(album);
+
+        Album resAlbum = albumService.getAlbum(saveAlbum.getAlbumId());
+        assertEquals("테스트",resAlbum.getAlbumName());
+    }
+}
