6aa98244202d434f949e631740cc5976939cd8fe
jooyoun lee <115621310+zwzwzw123@users.noreply.github.com>
2023-04-08 17:23:59 +0900

    add method to receive albumId as jsonbody

--- src/main/java/com/squarecross/photoalbum/controller/AlbumController.java
+++ src/main/java/com/squarecross/photoalbum/controller/AlbumController.java
@@ -5,10 +5,7 @@ import com.squarecross.photoalbum.service.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/albums")
@@ -20,6 +17,17 @@ public class AlbumController {
     @RequestMapping(value = "/{albumId}", method = RequestMethod.GET)
     public ResponseEntity<AlbumDto> getAlbum(@PathVariable("albumId") final long albumId){
         AlbumDto album = albumService.getAlbum(albumId);
+        return new ResponseEntity<>(album, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/query", method = RequestMethod.GET)
+    public  ResponseEntity<AlbumDto> getAlbumByQuery(@RequestParam final long albumId){
+        return new ResponseEntity<>(albumService.getAlbum(albumId), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/json_body")
+    public ResponseEntity<AlbumDto> getAlbumByJson(@RequestBody final AlbumDto albumDto){
+        AlbumDto album = albumService.getAlbum(albumDto.getAlbumId());
         return new ResponseEntity<>(album, HttpStatus.OK);
     }
 }
